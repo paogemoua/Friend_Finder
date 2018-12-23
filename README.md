71825ce184f30f181f5bf90f19093b015dc4291d
Paoge Moua <paoge.moua@gmail.com>
2018-12-23 01:36:17 -0600

    Updates to all files - minor changes and such

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# Friend_Finder
+# FriendFinder
+
+## Description
+FriendFinder is a compatibility-based "friend finder" application -- basically a dating app. This site will take in results from your users' surveys, then compare their answers with those from other users. The application will then display the name and picture of the user with the best overall match.
+
+## Visuals
+<!-- [Image][image] -->
+
+<!-- [![Bamazon](assets/images/Bamazon.png)](https://github.com/paogemoua/bamazon) -->
+
+## Demo
+	
+FriendFinder is deployed to Heroku. Please check it out via [this link]<!--(https://---.herokuapp.com/)-->.
+
+## Installation
+
+To install the application follow the instructions below:
+
+	git clone git@github.com:paogemoua/Friend_Finder.git
+	cd Friend_Finder
+	npm install
+	
+## Run Locally
+
+To run the application locally and access it in your browser, first set the `PORT` environment variable to the value of your choice. An example is shown below.
+
+	export PORT=3030
+	
+After the `PORT` environment variable has been set, run the Node.js application with the command below.
+
+	node server.js
+	
+The application will now be running locally on `PORT`, in this case that is port 3030. You can then access it locally from your browser at the URL `localhost:PORT`, in this case `localhost:3030`.
+
+## Technologies Used
+* HTML
+* CSS
+* Node.js
+* MySQL
+* npm packages
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Authors
+#### Meta
+Paoge Moua - primary author
+* Contact info: paoge.moua@gmail.com OR pobzeb.m@gmail.com
+* Portfolio address: https://paogemoua.github.io/
+* GitHub: https://github.com/paogemoua
+
+## License
+Distributed under the [MIT] license.
+
+## Reference
+
+## Project Status
+* 12.18.2018 Project open & minor updates - in progress
+
+<!-- Linked -->
+[MIT]: https://choosealicense.com/licenses/mit/
+<!-- [Watch the video for Bamazon demo]: https://drive.google.com/file/d/1UfLPqX6VsiCDgM2JOvPRmo-KcEcFeYOr/view -->
+<!-- [image]: https://drive.google.com/file/d/1-LnstPMlT99lfDg3XKhBpMbFZeaHqAp8/view?usp=sharing -->
---
+++ app/data/friend.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Garo the Golden Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/c/c1/Garo_crest2.png/revision/latest/scale-to-width-down/1000?cb=20130307173834',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Zero the Silver Fanged Knight',
+		photo: 'https://d1w8cc2yygc27j.cloudfront.net/4757814974131478939/1630513017370458450.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dan the Midnight Sun Knight',
+		photo: 'https://i.ebayimg.com/thumbs/images/g/SrYAAOSwio1ZmmgY/s-l200.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bado the Storm Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/f/fe/Bado_9.jpg/revision/latest/scale-to-width-down/1000?cb=20171030041609',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Baron the Thunder Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/7/73/Baron.jpg/revision/latest/scale-to-width-down/1000?cb=20160510020543',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Gai the Sky Bow Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/c/c3/Gai2.png/revision/latest?cb=20180727073249',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Zen the Flame Sword Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/9/9b/Zen_Render.png',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Crow the Phantom Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/9/9b/Crow_6.jpg/revision/latest?cb=20171106234944',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Giga the Beast Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/1/1f/Giga_2.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Roze the Glittering Shadow Knight',
+		photo: 'https://vignette.wikia.nocookie.net/garo/images/f/fd/Roze_03.jpg/revision/latest/scale-to-width-down/360?cb=20181006221036',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
